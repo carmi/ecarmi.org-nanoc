8267032a657bf32c9259c59cb537dc3ceb544538
Evan Carmi <evan@ecarmi.org>
2011-07-23 14:30:23 -0400

    Setup dev/prod settings files

---
+++ settings/prod.rb
@@ -0,0 +1,6 @@
+# PRODUCTION SETTINGS - settings file; loaded by rules
+$include_drafts = false
+$recent_posts_limit = 7
+$use_minified_js = true
+$use_minified_css = true
+$development_mode = false
