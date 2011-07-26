fe837eadb899fc39fc6eb822d9eaf72195887391
Evan Carmi <evan@ecarmi.org>
2011-07-26 00:57:11 -0400

    Restructured layouts

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -4,3 +4,10 @@ include Nanoc3::Helpers::LinkTo
 include Nanoc3::Helpers::Breadcrumbs
 include Nanoc3::Helpers::Rendering
 include Nanoc3::Helpers::XMLSitemap
+
+def featured_articles
+  articles = @items.select do |item|
+    item[:kind] == 'article' && item[:featured] == true
+  end
+  articles.sort_by { |post| attribute_to_time(post[:created_at])}.reverse
+end
