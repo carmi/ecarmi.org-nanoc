fc6e241265c05effb588ad95deeba918f2f7e522
Evan Carmi <evan@ecarmi.org>
2011-07-31 09:55:54 -0400

    Added settings.rb to .gitignore

--- settings.rb
+++
@@ -1,7 +0,0 @@
-#Settings file; loaded by rules
-$include_drafts = true
-$recent_posts_limit = 7
-$highlight_syntax = false
-$use_minified_js = false
-$use_minified_css = false
-$development_mode = true
