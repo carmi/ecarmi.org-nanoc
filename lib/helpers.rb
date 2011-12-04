ae256f3fab1cdd8b42adce66c03a1dac35180be3
Evan Carmi <evan@ecarmi.org>
2011-12-04 22:02:12 +0100

    Added yui compressor

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -41,3 +41,27 @@ def hashed_url(filename)
   item = @items.detect { |x| x.identifier.include?(filename.gsub(ext,'')) }
   return hashed_route(item) unless item.nil?
 end
+
+YUI_JAR = File.dirname(__FILE__) + "/../../tools/yuicompressor-2.4.7.jar"
+
+class YuiCompressor < Nanoc3::Filter
+  identifier :yui_compress
+  type :text => :binary
+  def run(content, params={})
+    type = type_from_extension
+    cmd = "java -jar #{YUI_JAR} --type #{type} -o #{output_filename}"
+    IO.popen(cmd, 'w') { |f| f.write(content) }
+    raise "yuicompressor exited with #{$?} for '#{cmd}'" unless $? == 0
+  end
+
+  def type_from_extension
+    case @item[:extension]
+    when /^css/
+      "css"
+    when /^js/
+      "js"
+    else
+      raise "unknown type for yuicompressor '#{@item[:extension]}'"
+    end
+  end
+end
