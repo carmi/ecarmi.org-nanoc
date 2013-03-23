3a35d47f2fa89ac0643471941ab6a2a27cf49b9a
Evan Carmi <evan@ecarmi.org>
2013-03-22 21:22:39 -0400

    Updated js

--- content/static/js/ecarmi.js
+++ content/static/js/ecarmi.js
@@ -39,3 +39,15 @@ $('nav.sidebar a.toggle').click(function() {
   });
   return false;
 });
+
+
+//// Portfolio masonry
+//var $container = $('div.portfolio div.grid');
+//$container.imagesLoaded(function(){
+//  $container.masonry({
+//    itemSelector : '.grid-item',
+//    columnWidth: function( containerWidth ) {
+//       return containerWidth / 2;
+//     }
+//  });
+//});
