5c685aaa8ff9a5ba888d6ce3e4d5a00cb9f93dc4
Evan Carmi <evan@ecarmi.org>
2014-11-02 11:55:54 -0800

    Add json

--- content/static/js/ecarmi.js
+++ content/static/js/ecarmi.js
@@ -4,8 +4,20 @@
  * @author Evan Carmi
  */
 
+var contact_form_submit = '#contact-form input[type=submit]';
+
 $(window).load(function() {
   $('.action-descriptions p:not(:first)').hide({duration: 0});
+  $(contact_form_submit).hide({duration: 0})
+
+  $('#contact-form *').bind('mouseover focus', function() {
+    $(contact_form_submit).show({duration: 0})
+  });
+
+  $(contact_form_submit).click(function(e) {
+    sendForm();
+    e.preventDefault();
+  });
 });
 
 var hideall = function(duration) {
@@ -18,3 +30,16 @@ $('.action a').click(function() {
   $(".action-descriptions ." + section).fadeIn(700);
   return false;
 });
+
+var sendForm = function() {
+  $.ajax({
+      url: $('#contact-form').attr('action'),
+      method: "POST",
+      data: $('form#contact-form').jsonify(),
+      dataType: "json",
+      success: function(){ $('#contact-form').replaceWith(success_html); },
+      error: function(xhr){ console.log(xhr); $('#contact-form').replaceWith('error'); }
+  });
+}
+
+var success_html = "<b>I got it! Thanks</b>"
